c47a85838d5b95218157e7d8d2b934930da9eaa0
FERCSA <FERCSA@users.noreply.github.com>
2018-07-14 20:42:19 +0200

    Update example.c

--- SM_demo/example.c
+++ SM_demo/example.c
@@ -2,7 +2,11 @@
  * #FCDS - Slave Module communication example for C
  * How-To read/writa data, just call the functions below.
  *
- * Run: gcc example.c -o example && ./example
+ * Default I2C address for a Slave Module is 0x08
+ * If you got more than one and it's not labeled, please set your new addresses one by one,
+ * calling set_i2c_address_for_slave_module()
+ *
+ * Compile and run: gcc example.c -o example && ./example
  *
  * More info: fercsa@freemail.hu - www.fercsa.com
  */
